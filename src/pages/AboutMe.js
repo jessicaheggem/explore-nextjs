dfec22d92702995e165f01b9fa31528c52b687f8
Jessica Heggem <jessicaheggem@gmail.com>
2025-01-24 13:21:08 -0600

    updated about me text and deleted nextjs app home text

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -12,7 +12,20 @@ export default function AboutMe() {
       <main>
         <div>
           <h1>About Me</h1>
-          <p>This is the page where I tell a million things about my wonderful self. My accomplishments. My skills. What I can do for your company. I am basically amazing. Highly recommend hiring me.</p>
+          <p>My name is Jessica and I&apos;m someone who loves to learn. 
+            I&apos;m experienced in front end software development and business analytics. 
+            Currently, I&apos;m looking for opportunities in both areas. 
+            My emphasis is on Business Analyst roles while I ramp up my software development portfolio 
+            since I stepped away from a software development specific role coding for about 2 years. 
+            I used SQL in Snowflake and MS SQL Server to run queries daily while I was at SPS Commerce, 
+            so the knowledge hasn&apos;t completely left my mind. 
+            It&apos;s exciting to dive back into React, specifically Next.js, in this case, 
+            because I get to learn how to navigate coding in a 
+            world where AI exists, since it didn&apos;t exist when I was a developer. 
+            Weirdly enough, ChatGPT launched 19 days after I was laid off from my dev role in 2022. 
+            Alas, I have rambled enough now. This is simply placeholder text
+            until I make my way back to add my resume and trim this text down 
+            to be shorter and sweeter. Thanks for tuning in!</p>
         </div>
       </main>
     </div>
