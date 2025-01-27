66d264fe8e9aeb1b7c31c0bb6b762ffbed527ca5
Jessica Heggem <jessicaheggem@gmail.com>
2025-01-27 09:17:31 -0600

    figuring out scss bootstrap theme overwrite - still troubleshoooting a bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
-import 'bootstrap/dist/css/bootstrap.css';
-
+import "@/styles/customBootstrap.scss";
+/* import 'bootstrap/dist/css/bootstrap.css';
+ */
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
